6166b300e5b41de24a414a276721f5d1b3020169
Ankit Bansal <ankitbansal032003@gmail.com>
2025-08-23 17:03:43 +0530

    Delete Array directory

--- Array/array_traversal.java
+++
@@ -1,8 +0,0 @@
-public class array_traversal {
-    public static void main(String[] args) {
-        int[] a = new int[100];
-        int[] list = {1,2,3,4,5,6,7,8,9};
-        System.out.println(list.length);
-        System.out.println(a.length);
-    }
-}
--- Array/shorthand_assignment.java
+++
@@ -1,9 +0,0 @@
-public class shorthand_assignment {
-    public static void main(String[] args) {
-        int a = 20;
-        int b = 5;
-
-        a %= b;
-        System.out.println(a);
-    }
-}

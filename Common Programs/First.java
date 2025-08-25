25af2c7d407d2eeb09f8bbe61144cb28381af6de
Ankit Bansal <ankitbansal032003@gmail.com>
2025-08-25 20:02:18 +0530

    Update First.java

--- Common Programs/First.java
+++ Common Programs/First.java
@@ -3,7 +3,11 @@ package Common_Programs;
 import java.lang.*;
 
 public class First{
+  
   public static void main(String[] args) {
+    
     System.out.println("welcome to Miet");
+    
   }
+
 }	
